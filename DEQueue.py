28218b5f013f6afa0b7500a0d8940b29160334d4
Mikhil <mikhil.nagarale@gmail.com>
2018-02-18 17:06:31 +0530

    Updating function insert_at_front() & delete_from_back()

--- DEQueue.py
+++ DEQueue.py
@@ -37,23 +37,24 @@ class DEQueue:
         if self.rear == self.front:
             print("Queue Underflow!")
         else:
-            self.rear = self.rear + 1
+            self.rear = self.rear - 1
             self.queue[self.rear] = 0
 
     def insert_at_front(self,element):
         if self.rear == self.arraySize:
             print ("Queue Overflow!")
         else:
-            self.rear = self.rear + 1
             for i in range(self.rear,self.front,-1):
+                print ("value of i=" + str(i))
                 self.queue[i] = self.queue[i-1]
+            self.rear = self.rear + 1
             self.queue[self.front] = element
 
     def delete_from_front(self):
         if self.rear == self.front:
             print("Queue Underflow!")
         else:
-            print("deleting element=" + str(self.queue[self.self.front]))
+            print("deleting element=" + str(self.queue[self.front]))
             self.queue[self.front] = 0
             self.front = self.front + 1
 
