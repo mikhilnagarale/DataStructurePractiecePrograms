b83c6cb64ee811c8e9154063c810f9d3273dae85
Mikhil Nagarale <13365191+mikhilnagarale@users.noreply.github.com>
2018-06-24 12:37:35 +0530

    Creating file Heap.py with heap implementation

--- Heap.py
+++ Heap.py
@@ -25,3 +25,91 @@
 #Only thing we need to consider is every parent should be either greater than (max heap) child or less than (min heap) child.
 #Hence different datastructure are used to store heap(array) and BST(pointer based nodes) .
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Heap:
+    def __init__(self,heapType):
+        self.heapList = [0]
+        self.heapsize = 0
+        self.heapType = heapType
+        
+    def __repr__(self):
+        return str(self.heapList)
+    
+    def pushUp(self,index):
+        while index // 2 > 0:
+            #Comparing newly added leaf element with it's parent node
+            if self.heapList[index//2] > self.heapList[index]:
+                temp = self.heapList[index//2]
+                self.heapList[index//2] = self.heapList[index]
+                self.heapList[index] = temp
+            index = index//2
+
+    def getMinChild(self,index):
+        if self.heapsize == 2*index:
+            print('mc='+str(2*index))
+            return 2*index
+        elif self.heapList[2*index] < self.heapList[2*index+1]:
+            print('mc='+str(2*index))
+            return 2*index
+        else:
+            print('mc='+str(2*index+1))
+            return 2*index+1
+        
+    def pushDown(self,index):
+        while self.heapsize >= 2*index:
+            mc = self.getMinChild(index)
+            if self.heapList[index] > self.heapList[mc]:
+                temp = self.heapList[mc]
+                self.heapList[mc] = self.heapList[index]
+                self.heapList[index] = temp
+                print(str(self.heapList))
+            index = mc
+            print ('heapseze='+str(self.heapsize)+' index='+str(index))
+            
+        
+        
+        
+    def delete(self):
+        print("Deleting the minimum element")
+        print(str(self.heapList))
+        temp = self.heapList[1]
+        self.heapList[1] = self.heapList[self.heapsize]
+        print(str(self.heapList))
+        self.heapList.pop()
+        self.heapsize = self.heapsize - 1
+        self.pushDown(1)
+        
+    def buildHeap(self,sampleList):
+        index = len(sampleList)//2
+        self.heapsize = len(sampleList)
+        self.heapList = [0]+sampleList
+        while index > 0:
+            print ('index='+str(index))
+            self.pushDown(index)
+            index = index - 1
+        
+    
+    def insert(self,val):
+        self.heapList.append(val)
+        self.heapsize = self.heapsize + 1
+        self.pushUp(self.heapsize)
+
+if __name__=="__main__":
+    myHeap = Heap('m')        
+    print (myHeap)
+    myHeap.insert(20)
+    myHeap.insert(10)
+    myHeap.insert(15)
+    myHeap.insert(13)
+    myHeap.insert(9)
+    print (myHeap)
+    myHeap.insert(25)
+    myHeap.insert(17)
+    myHeap.insert(4)
+    print(myHeap)
+    myHeap.delete()
+    print(myHeap)
+    myList = [9, 5, 6, 2, 3]
+    myHeap.buildHeap(myList)
+    print(myHeap)
+
+    
