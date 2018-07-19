13970097a5ad9eff45fbee9daa4ad5b8211e40b7
Mikhil Nagarale <13365191+mikhilnagarale@users.noreply.github.com>
2018-07-19 10:44:42 +0530

    Updated SequentialSearch.py for ordered search
    #Added Reference Link for Understanding.
    #Added method orderedSequentialSearch() for searching in an ordered list.
    Below are the Complexity details-
    1)Method one sequentialSearch()
                          Best     worst    average
    found             1           n            n/2
    not found       n           n            n
    Final Complexity O(n)
    2)Method one orderedSequentialSearch()
                          Best     worst    average
    found             1           n            n/2
    not found       1           n            n/2
    Final Complexity O(n)
    
    In both the scenarios complexity in O(n) for sequential Search.

--- SequentialSearch.py
+++ SequentialSearch.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+#Ref: Logic & Implementation: http://interactivepython.org/courselib/static/pythonds/SortSearch/TheSequentialSearch.html
+
 def sequentialSearch(alist,val):
     pos = 0 
     found =  False
@@ -10,7 +12,30 @@ def sequentialSearch(alist,val):
             pos = pos + 1
     return found
 
+def orderedSequentialSearch(alist,val):
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(alist) and not found and not stop:
+        #print('checking element '+str(alist[pos]))
+        if (alist[pos]==val):
+            found = True
+        elif alist[pos] > val:
+            stop = True
+        else:
+            pos = pos + 1
+    return found        
+    
 if __name__=="__main__":
+    print('Checking element search for non-ordered list')
     myList = [3,9,4,5,346,456,567,56,76,77,876,8,769,78,89,3,24,42,42,4,2342]
     if sequentialSearch(myList,42):
         print('Your search is present in list!')
+    print('Checking element search for ordered list')
+    myListNew = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20]
+    if orderedSequentialSearch(myListNew,10):
+        print('Your search is present in list!')
+    else:
+        print('Your search is not present in list!')
+    
+    
