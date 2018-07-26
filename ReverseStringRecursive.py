14a316b2acdf118b05edb202b1c28022e532e245
Mikhil Nagarale <13365191+mikhilnagarale@users.noreply.github.com>
2018-07-26 12:10:17 +0530

    Updating Question

--- ReverseStringRecursive.py
+++ ReverseStringRecursive.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #Ref: http://interactivepython.org/courselib/static/pythonds/Recursion/WhatIsRecursion.html
 
+#Q: Write a function that takes a string as a parameter and returns a new string that is the reverse of the old string.
+
 def rev(val):
     if len(val)==1:
         return val[0]
